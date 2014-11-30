ca7399f17a383279e4f9c3e920de029eca5f6bd8
Lorin Hochstein <lorin.hochstein@sendgrid.com>
2014-11-29 20:57:02 -0500

    Set UTC as the time zone

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ ALLOWED_HOSTS = []
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = 'UTC'
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
